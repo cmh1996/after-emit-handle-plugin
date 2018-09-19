fd68b89be7a22cebabde7320fb10b975d06163f0
cmh1996 <752380178@qq.com>
2018-09-20 00:07:03 +0800

    fix bugs

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ var path = require('path');
 var request = require('request');
 
 //生成打包后的文件信息列表
-function getFileList(dir, fileList) {
+function getFileList(dir) {
     var files = fs.readdirSync(dir);
-    
-    fileList = fileList || [];
+    var fileList = [];
 
     files.forEach(function(file) {
         var filePath = path.join(dir, file);
         if (fs.statSync(filePath).isDirectory()) {
-            var childList = getFileList(filePath, []);
+            var childList = getFileList(filePath);
             fileList.push({
                 [file]: childList
             });
@@ -19,54 +18,57 @@ function getFileList(dir, fileList) {
             fileList.push(file);
         }
     });
-    return JSON.stringify(fileList, null, 2);
+    return fileList;
 };
 
 //上传文件
 function uploadFile(url,fileList,callback){
-	var fd = [];
-	for(var i=0;i<fileList.length;i++){
-		fd.push({
-			key:fileList[i].name,
-			value:fs.createReadStream(fileList[i].path)
-		});
-	}
-	request.post({url,fd},function(err,res){
-		if(err){
-			callback(err);
-			return;
-		}
-		callback();
-	})
+    var fd = [];
+    for(var i=0;i<fileList.length;i++){
+        fd.push({
+            key:fileList[i].name,
+            value:fs.createReadStream(fileList[i].path)
+        });
+    }
+    request.post({url,fd},function(err,res){
+        if(err){
+            callback(err);
+            return;
+        }
+        callback();
+    })
 }
 
 //一个打包完成后，生成文件信息json，并把打包后的文件发布到指定位置的plugin
 function AfterEmitHandlePlugin(options){
-	this.jsonPath = options.jsonPath;//生成的json文件存放的位置
-	this.url = options.url;//发送到该url
+    this.jsonPath = options.jsonPath;//生成的json文件存放的位置
+    this.url = options.url;//发送到该url
 }
 
 AfterEmitHandlePlugin.prototype.apply = function(compiler){
-	var jsonPath = this.jsonPath;
-	var url = this.url;
+    var jsonPath = this.jsonPath;
+    var url = this.url;
 
-	//将内存中的assets内容写入到磁盘后触发
-	compiler.plugin('after-emit',function(compilation,callback){
-		//生成文件信息json
-		var distList = getFileList('./dist', []);
+    //将内存中的assets内容写入到磁盘后触发
+    compiler.plugin('after-emit',function(compilation,callback){
+        //生成文件信息json
+        var distList = getFileList('./dist', []);
+        distList = JSON.stringify(distList,null,2);
         fs.writeFile(jsonPath,distList,{},function(err){
-        	if(err) throw err;
+            if(err){
+                console.log(err);
+            }
         })
 
         //传输文件
         var fileList = [];
         for(var key in compilation.assets){
-        	if(compilation.assets[key].existsAt){
-        		fileList.push({name:key,path:compilation.assets[key].existsAt})
-        	}
+            if(compilation.assets[key].existsAt){
+                fileList.push({name:key,path:compilation.assets[key].existsAt})
+            }
         }
         uploadFile(url,fileList,callback);
-	})
+    })
 }
 
 module.exports = AfterEmitHandlePlugin;
